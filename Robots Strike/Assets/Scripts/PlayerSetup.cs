52a2e9f21d01c2e11b5f0410417e0d1016f30090
Idzik <iddziku@gmail.com>
2018-08-21 17:17:42 +0200

    Synchronizacja ruchów

---
+++ Robots Strike/Assets/Scripts/PlayerSetup.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+// dziedziczymy z klasy NetworkBehaviour
+public class PlayerSetup : NetworkBehaviour
+{
+    [SerializeField]
+    Behaviour[] componentsToDisable;
+
+    Camera sceneCamera;
+
+    private void Start()
+    {
+        // jeżeli nie jesteśmy lokalnym graczem(tym którym sterujemy) to wyłączmy komponenty 
+        // ruchu itd u innych postaci żeby nie było tak, że jak jedna postać się przemieszcza
+        // to i druga to robi...
+        if (!isLocalPlayer)
+        {
+            for(int i = 0; i < componentsToDisable.Length; i++)
+            {
+                componentsToDisable[i].enabled = false;
+            }
+        }
+        else
+        {   // jeżeli wchodzimy do gry, wyłącz kamerę lokalną
+            // (tą, która patrzy od góry)
+            sceneCamera = Camera.main;
+
+            if(sceneCamera != null)
+            {
+                sceneCamera.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    private void OnDisable()
+    {
+        // jeżeli wychodzimy z gry, włącz kamerę lokalną
+        if(sceneCamera != null)
+        {
+            sceneCamera.gameObject.SetActive(true);
+        }
+    }
+}
