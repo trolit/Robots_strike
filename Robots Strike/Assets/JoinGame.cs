5ba3c9f2f4528ee6c2e86bab0cb17a8e9c2268ca
Idzik <iddziku@gmail.com>
2018-09-01 00:42:30 +0200

    Dolaczanie cz2

---
+++ Robots Strike/Assets/JoinGame.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using UnityEngine.Networking.Match;
+
+public class JoinGame : MonoBehaviour
+{
+    List<GameObject> roomList = new List<GameObject>();
+
+    [SerializeField]
+    private Text status;
+
+    [SerializeField]
+    private GameObject roomListItemPrefab;
+
+    [SerializeField]
+    private Transform roomListParent;
+
+    private NetworkManager networkManager;
+
+    private void Start()
+    {
+        networkManager = NetworkManager.singleton;
+
+        if(networkManager.matchMaker == null)
+        {
+            networkManager.StartMatchMaker();
+        }
+
+        RefreshRoomList();
+    }
+
+    public void RefeshRoomList()
+    {
+        networkManager.matchMaker.ListMatches(0, 20, "", false, 0, 0, OnMatchList);
+        status.text = "Loading...";
+    }
+
+    public void OnMatchList(bool success, string extendedInfo, List<MatchInfoSnapshot> matchList)
+    {
+        status.text = "";
+
+        if(matchList == null)
+        {
+            status.text = "Couldnt get room list!";
+            return;
+        }
+
+        ClearRoomList();
+
+        foreach(MatchInfoSnapshot match in matchList)
+        {
+            GameObject _roomListItemGO = Instantiate(roomListItemPrefab);
+            _roomListItemGO.transform.SetParent(roomListParent);
+
+            // have a component sit on the gameobject that will take care of 
+            // player amount, room name and callback function to join the game
+        }
+    }
+
+    void ClearRoomList()
+    {
+        for (int i = 0; i < roomList.Count; i++)
+        {
+            Destroy(roomList[i]);
+        }
+
+        // remove references
+        roomList.Clear();
+    }
+}
