86c283828d7c30942cefdf027063d79b13e22d01
Idzik <iddziku@gmail.com>
2018-08-31 22:13:37 +0200

    hostowanie rozgrywki cz1

---
+++ Robots Strike/Assets/HostGame.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class HostGame : MonoBehaviour
+{
+    [SerializeField]
+    private uint roomSize = 6;
+
+    private string roomName;
+
+    private NetworkManager networkManager;
+
+    private void Start()
+    {
+        networkManager = NetworkManager.singleton;
+
+        // if matchMaker is not enabled
+        if(networkManager.matchMaker == null)
+        {
+            networkManager.StartMatchMaker();
+        }
+    }
+
+    public void SetRoomName(string _name)
+    {
+        roomName = _name;
+    }
+
+    public void CreateRoom()
+    {
+        if(roomName != "" && roomName != null)
+        {
+            Debug.Log("Creating a Room" + roomName);
+
+            // Create room
+            networkManager.matchMaker.CreateMatch(roomName, roomSize, true, "", "", "", 0, 0, networkManager.OnMatchCreate);
+        }
+    }
+
+}
